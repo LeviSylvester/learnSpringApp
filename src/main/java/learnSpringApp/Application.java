7764b3279f769fb0ab2356b9974fc4978710889d
Levi Szilveszter <l3vi.sylvester@gmail.com>
2020-02-16 11:54:29 +0200

    converted to spring boot app

--- src/main/java/learnSpringApp/Application.java
+++ src/main/java/learnSpringApp/Application.java
@@ -1,17 +1,30 @@
 package learnSpringApp;
 
 import learnSpringApp.config.ApplicationConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Application {
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private SchoolManager schoolManager;
 
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-        applicationContext.register(ApplicationConfiguration.class);
-        applicationContext.refresh();
-
-        SchoolManager schoolManager = (SchoolManager) applicationContext.getBean("schoolManager");
+    @Override
+    public void run(String... args) throws Exception {
+//        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+//        applicationContext.register(ApplicationConfiguration.class);
+//        applicationContext.refresh();
+//
+//        SchoolManager schoolManager = (SchoolManager) applicationContext.getBean("schoolManager");
         schoolManager.saveData();
+        System.out.println("\nSpring boot works here as well!");
     }
 }
